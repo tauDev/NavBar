1c7031d1dad61b4d61a6cb8dca6a9f5dcab4c71a
Parinya <parinyat0926@gmail.com>
2018-06-04 09:29:59 +0700

    Initial commit

---
+++ navbar/src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Boots from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Navbar from '@/components/Navbar'
+
+Vue.use(Router)
+Vue.use(Boots)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/Navbar',
+      name: 'Navbar',
+      component: Navbar
+    }
+  ]
+})
